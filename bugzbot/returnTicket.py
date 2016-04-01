e1b1a9241426ab8e682264bee5d5f9c3dced8cc5
Jon Nagel <j.nagelxz@gmail.com>
2016-04-01 16:58:50 -0400

    add last event

--- bugzbot/returnTicket.py
+++ bugzbot/returnTicket.py
@@ -4,11 +4,12 @@ import creds
 
 def returnTicket(ticketNum):
 	fb = FogBugz(creds.URL, creds.TOKEN)
-	retrieveTicket = fb.search(q=ticketNum, cols="ixBug,sTitle,sPersonAssignedTo")
+	retrieveTicket = fb.search(q=ticketNum, cols="ixBug,sTitle,sPersonAssignedTo,sLatestTextSummary")
 
 	ticketTitle = retrieveTicket.cases.case.stitle.string
 	ticketOwner = retrieveTicket.cases.case.spersonassignedto.string
 	ticketURL   = "https://tenthwave.fogbugz.com/f/cases/"+ticketNum
+	ticketLastUpdate = retrieveTicket.cases.case.sLatestTextSummary.string
 	ticket = ticketURL + "\n\nTicket: " + ticketNum + "\n" + ticketTitle
 
  	return ticket
