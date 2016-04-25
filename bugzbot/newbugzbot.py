fdc0f73732fba48f95410b27ae23a93a061ff2c7
Jon Nagel <j.nagelxz@gmail.com>
2016-04-25 18:22:20 -0400

    working new bugzbot with assign.still rough

--- bugzbot/newbugzbot.py
+++ bugzbot/newbugzbot.py
@@ -11,63 +11,76 @@ class Bugz(object):
 
     def __init__(self):
         self.config = yaml.load(file('bugzbot.conf', 'r'))
-        self.fb = FogBugz(self.config['FOGBUGZ_URL'], self.config['FOGBUGZ_TOKEN'])
+        self.fb = FogBugz(self.config['FOGBUGZ_URL'],
+            self.config['FOGBUGZ_TOKEN'])
 
     def returnUser(self, _semail):
-        user = self.fb.viewPerson(semail=_semail)
-
+        self.user = self.fb.viewPerson(semail=_semail)
         try:
-            self.fb_user = user.people.person.sfullname.string
+            self.fb_user = self.user.people.person.sfullname.string
             return self.fb_user
         except AttributeError:
             return "User not found"
 
-    def assignTicket(self, ticket_num, user, orig_user, ):
+    def assignTicket(self, ticket_num, user, orig_user):
+        self.touser = self.returnUser(user)
+        print user
+        print self.touser
+        if self.touser != "User not found":
+            self.event = "This ticket was assigned to you by slack user " + \
+                orig_user[1] + " courtesy of bugzbot!"
+            self.fb.edit(ixBug=ticket_num,
+                sPersonAssignedTo=self.touser,
+                sEvent=self.event)
 
-        touser = self.returnUser(user[2])
-        fromuser = self.returnUser(orig_user[2])
+            self.checker = self.fb.search(q=ticket_num, cols="ixPersonAssignedTo")
+            print self.checker
 
-        if touser and fromuser != "User not found":
-            event = "This ticket was assigned to you by slack user " + orig_user[1] + " courtesy of bugzbot!"
-            # fb.edit(ixBug=ticket_num, sPersonAssignedTo=touser, sEvent=event)
+            if self.checker.cases.case.ixpersonassignedto.string != "93":
+                return "success"
+            else:
+                return "One of us screwed up. I got stuck with the ticket somehow." \
+                    "Please follow up and manually assign the ticket so it does not get lost in the shuffle!"
+        else:
+            return "Fogbugz user was not found. Please try again."
 
     def returnTicket(self, option, ticket_num):
         if option:
+            self.retrieve_ticket = self.fb.search(q=ticket_num,
+                cols='ixBug,sTitle,sProject')
 
-            retrieve_ticket = self.fb.search(q=ticket_num, cols='ixBug,sTitle,sProject')
+            if self.retrieve_ticket.cases['count'] == str(1):
 
-            if retrieve_ticket.cases['count'] == str(1):
+                self.ticket_title = self.retrieve_ticket.cases.case.stitle.string
+                self.ticket_project = self.retrieve_ticket.cases.case.sproject.string
+                self.ticket_URL = "https://tenthwave.fogbugz.com/f/cases/" + ticket_num
 
-                ticket_title = retrieve_ticket.cases.case.stitle.string
-                ticket_project = retrieve_ticket.cases.case.sproject.string
-                ticket_URL = "https://tenthwave.fogbugz.com/f/cases/" + ticket_num
-
-                ticket = "Ticket: " + ticket_num + "  |  " + ticket_title + "\n\n" + ticket_URL
-                print ticket
-                return ticket
+                self.ticket = "Ticket: " + ticket_num + "  |  " + self.ticket_title + \
+                    "\n\n" + self.ticket_URL
+                print self.ticket
+                return self.ticket
             else:
                 msg = "Something went wrong! Either Fogbugz is down, you're a magician calling a number " \
                     "that does not exist yet, or it somehow returned more than one ticket."
                 print msg
                 return msg
         else:
-            retrieve_ticket = self.fb.search(
+            self.retrieve_ticket = self.fb.search(
                 q=ticket_num,
-                cols="ixBug,sTitle,sLatestTextSummary,sArea,sProject"
-            )
+                cols="ixBug,sTitle,sLatestTextSummary,sArea,sProject")
 
-            if retrieve_ticket.cases['count'] == str(1):
+            if self.retrieve_ticket.cases['count'] == str(1):
 
-                ticket_title = retrieve_ticket.cases.case.stitle.string
-                ticket_area = retrieve_ticket.cases.case.sarea.string
-                ticket_project = retrieve_ticket.cases.case.sproject.string
-                ticket_URL = "https://tenthwave.fogbugz.com/f/cases/" + ticket_num
-                ticket_last_update = retrieve_ticket.cases.case.slatesttextsummary.string
+                self.ticket_title = self.retrieve_ticket.cases.case.stitle.string
+                self.ticket_area = self.retrieve_ticket.cases.case.sarea.string
+                self.ticket_project = self.retrieve_ticket.cases.case.sproject.string
+                self.ticket_URL = "https://tenthwave.fogbugz.com/f/cases/" + ticket_num
+                self.ticket_last_update = self.retrieve_ticket.cases.case.slatesttextsummary.string
 
-                ticket = ticket_URL + "\n\nTicket: " + ticket_num + "\n" + ticket_project + " : " + \
-                    ticket_area + "\n*" + ticket_title + "*\n\n" + ticket_last_update
-                print ticket
-                return ticket
+                self.ticket = self.ticket_URL + "\n\nTicket: " + ticket_num + "\n" + self.ticket_project + \
+                    " : " + self.ticket_area + "\n*" + self.ticket_title + "*\n\n" + self.ticket_last_update
+                print self.ticket
+                return self.ticket
             else:
                 msg = "Something went wrong! Either Fogbugz is down, you're a magician calling a number " \
                     "that does not exist yet, or it somehow returned more than one ticket."
@@ -81,7 +94,7 @@ class MessageProcessor(object):
         self.split_message = []
         self.touser = None
 
-    def processMessage(self, message, ticket_num, orig_user, channel):
+    def processMessage(self, message, ticket_num, orig_user, _channel):
         self.split_message = shlex.split(message)
         self.options = self.processOptions(self.split_message)
 
@@ -94,8 +107,25 @@ class MessageProcessor(object):
                 if re.match(r'(?:<@\w.*?>)', msg):
                     self.touser = re.search(r'((?<=\<@)\w*)', msg).group(0)
                     print self.touser
-        #if self.options[2]: #assignto
-            #do later
+
+        if self.options[2]:  # assignto
+            #  this is going to be fun...
+            if not self.options[3]:
+                self.touser_email = sc.find_user(self.touser)[2]
+                self._orig_user = sc.find_user(orig_user)
+                self.ticket = Bugz().assignTicket(ticket_num, self.touser_email, self._orig_user)
+
+                if self.ticket == "User not Found":
+                    sc.client.api_call('chat.postMessage', channel=_channel,
+                        as_user=True, text="Something went wrong and the ticket could not be assigned" +
+                            " to that user for some reason, make sure it did not get stuck in the API user's list.")
+                else:
+                    self.fromuser_name = sc.find_user(orig_user)[1]
+
+                    sc.client.api_call('chat.postMessage', channel=self.touser,
+                        as_user=True, text="Slack user " + self.fromuser_name + " assigned you a ticket on Fogbugz!")
+                    sc.client.api_call('chat.postMessage', channel=_channel,
+                        as_user=True, text="The ticket you requested has been sent to the Slack user on Fogbugz.")
 
         if self.options[1]:  # sendto
 
@@ -105,12 +135,13 @@ class MessageProcessor(object):
                 self.ticket = Bugz().returnTicket(False, ticket_num)
 
             try:
-                fromuser = sc.find_user(orig_user)
+                self.fromuser = sc.find_user(orig_user)
 
                 if self.ticket.startswith("Something"):
-                    sc.client.api_call('chat.postMessage', channel=from_user[0], as_user=True, text=self.ticket)
+                    sc.client.api_call('chat.postMessage', channel=self.from_user[0],
+                        as_user=True, text=self.ticket)
                 else:
-                    self.msg = "This ticket was sent to you by:  " + fromuser[1] + "\n\n" + self.ticket
+                    self.msg = "This ticket was sent to you by:  " + self.fromuser[1] + "\n\n" + self.ticket
                     sc.client.api_call('chat.postMessage', channel=self.touser, as_user=True, text=self.msg)
 
             except AttributeError:
@@ -142,6 +173,7 @@ class MessageProcessor(object):
         self._short = False
         self._sendto = False
         self._assignto = False
+        self._fullname = False
 
         for msg in message:
 
@@ -151,8 +183,10 @@ class MessageProcessor(object):
                 self._sendto = True
             elif re.match(r'(-assignto)', msg):
                 self._assignto = True
+            elif re.match(r'(-fullname)', msg):
+                self._name = True
 
-        return [self._short, self._sendto, self._assignto]
+        return [self._short, self._sendto, self._assignto, self._fullname]
 
 
 class Slack(object):
@@ -191,12 +225,11 @@ try:
                     orig_user = new_reply['user']
                     channel = new_reply['channel']
 
-                    if len(re.findall(r'!((bugz|bugzid|case|ticket|ticekt).*?(\d{4,6}))', message)) > 0:
-                        tickets = re.findall(r'!((bugz|bugzid|case|ticket|ticekt).*?(\d{4,6}))', message)
+                    if len(re.findall(r'!((bugz|case|ticket|ticekt).*?(\d{4,6}))', message)) > 0:
+                        tickets = re.findall(r'!((bugz|case|ticket|ticekt).*?(\d{4,6}))', message)
                         for ticket in tickets:
-
+                            print ticket
                             ticket_num = ticket[2]
-
                             msg = MessageProcessor()
                             msg.processMessage(message, ticket_num, orig_user, channel)
 
